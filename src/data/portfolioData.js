7b07d11818cc30a51653ba441e3dcaa297647c54
June Yun <97yunjy@gmail.com>
2026-08-12 11:15:06 -0600

    Implement main.jsx and data

---
+++ src/data/portfolioData.js
@@ -0,0 +1,49 @@
+export const portfolioProjects = [
+  {
+    id: "shoes-market",
+    name: "Online Shoes Market",
+    type: "javascript",
+    path: "/projects/shoes-market",
+  },
+  {
+    id: "sorting-visualizer",
+    name: "Sorting Algorithm Visualizer",
+    type: "javascript",
+    path: "/projects/sorting-visualizer",
+  },
+  {
+    id: "class-registration",
+    name: "Class Registration Website",
+    type: "web",
+    path: "/projects/class-registration",
+  },
+  {
+    id: "qa-mobile",
+    name: "Q&A Mobile Application",
+    type: "mobile",
+    path: "/projects/qa-mobile",
+  },
+];
+
+export const aboutItems = [
+  {
+    id: "about",
+    name: "About Me",
+    path: "/",
+  },
+  {
+    id: "skills",
+    name: "Skills",
+    path: "/skills",
+  },
+  {
+    id: "experience",
+    name: "Experience",
+    path: "/experience",
+  },
+  {
+    id: "education",
+    name: "Education",
+    path: "/education",
+  },
+];
