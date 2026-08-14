849979a10820381dc030ed1351d1b5dc23fee9f8
June Yun <97yunjy@gmail.com>
2026-08-13 21:36:13 -0600

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import ActivityBar from "./components/layout/ActivityBar";
 import Explorer from "./components/layout/Explorer";
 import EditorTabs from "./components/layout/EditorTabs";
 import StatusBar from "./components/layout/StatusBar";
+
 import HomePage from "./components/home/HomePage";
+import SkillsPage from "./pages/SkillsPage";
 
 function App() {
   return (
@@ -14,14 +17,16 @@ function App() {
 
       <div className="portfolio-workspace">
         <ActivityBar />
-
         <Explorer />
 
         <section className="editor">
           <EditorTabs />
 
           <div className="editor__content">
-            <HomePage />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/skills" element={<SkillsPage />} />
+            </Routes>
           </div>
         </section>
       </div>
